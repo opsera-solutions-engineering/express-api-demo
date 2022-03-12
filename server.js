8237918ee268e04c4ae7fb9b19c42efad2ac7ce0
Derek Ross <dross91786@gmail.com>
2022-03-12 16:08:34 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+let currentDate = new Date()
+let cDay = currentDate.getDate()
+let cMonth = currentDate.getMonth() + 1
+let cYear = currentDate.getFullYear()
+let dateString = cMonth + '/' + cDay + '/' + cYear
+const CUSTOMER = process.env.CUSTOMER || 'Opsera'
+
+app.get('/helloWorld', function(req, res){
+   res.json({ msg: 'Hello World!', customer: CUSTOMER, date: dateString });
+});
+
+const PORT = process.env.PORT || 3000
+const HOST = process.env.HOST || '0.0.0.0';
+
+app.listen(PORT, HOST, () => console.log(`Server listening: ${HOST}:${PORT}`));
